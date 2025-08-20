3a5b99280ad5678b5131d19ded6b982f07fb5e48
ArefOrumiehei <areforumiehei@gmail.com>
2025-08-20 16:43:54 +0330

    Configuration axios

---
+++ frontend/src/api/axiosInstance.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}`,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
+axiosInstance.interceptors.request.use(config => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers["x-auth-token"] = token;
+  return config;
+});
+
+export default axiosInstance;
