f96c805628bde6036f37174f03aca33649cbc557
ArefOrumiehei <areforumiehei@gmail.com>
2025-08-20 16:36:43 +0330

    Create main layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+// Components
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import BackgroundCircles from "./animations/BackgroundCircles";
+
+// Styles
+import "./App.css"
+
+function App() {
+  const [userLogged, setUserLogged] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) setUserLogged(true);
+  }, []);
+
+  return (
+    <>
+      <BackgroundCircles />
+      {userLogged ? <Home /> : <Register onLogin={() => setUserLogged(true)} />}
+    </>
+  );
+}
+
+export default App;
